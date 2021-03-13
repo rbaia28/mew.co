21fed12bf8064fea8759e2f92c8324a7290f9d23
Bill Caffery <billythakidd04@gmail.com>
2021-03-13 13:57:13 -0500

    added symfony .gitignore files

--- src/User/Admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Admin extends User {
-	public function __construct($firstName, $lastName, $email, $password)
-	{
-		parent::__construct($firstName, $lastName, $email, $password, 'admin');
-	}
-}
