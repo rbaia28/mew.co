7319d3833ffb059d72ced552ba6db2e0d4780ec3
Bill Caffery <billythakidd04@gmail.com>
2021-03-19 01:32:49 -0400

    added setters and getters to pokemon entity class

--- src/Entity/Pokemon.php
+++ src/Entity/Pokemon.php
@@ -85,4 +85,230 @@ class Pokemon
     private $isArchived = '0';
 
 
+
+    /**
+     * Get the value of id
+     *
+     * @return  int
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of uuid
+     *
+     * @return  int
+     */ 
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * Set the value of uuid
+     *
+     * @param  int  $uuid
+     *
+     * @return  self
+     */ 
+    public function setUuid(int $uuid)
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dexId
+     *
+     * @return  int
+     */ 
+    public function getDexId()
+    {
+        return $this->dexId;
+    }
+
+    /**
+     * Set the value of dexId
+     *
+     * @param  int  $dexId
+     *
+     * @return  self
+     */ 
+    public function setDexId(int $dexId)
+    {
+        $this->dexId = $dexId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nickname
+     *
+     * @return  string
+     */ 
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+    /**
+     * Set the value of nickname
+     *
+     * @param  string  $nickname
+     *
+     * @return  self
+     */ 
+    public function setNickname(string $nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of isShiny
+     *
+     * @return  bool
+     */ 
+    public function getIsShiny()
+    {
+        return $this->isShiny;
+    }
+
+    /**
+     * Set the value of isShiny
+     *
+     * @param  bool  $isShiny
+     *
+     * @return  self
+     */ 
+    public function setIsShiny(bool $isShiny)
+    {
+        $this->isShiny = $isShiny;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cp
+     *
+     * @return  int
+     */ 
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * Set the value of cp
+     *
+     * @param  int  $cp
+     *
+     * @return  self
+     */ 
+    public function setCp(int $cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of currentLevel
+     *
+     * @return  int
+     */ 
+    public function getCurrentLevel()
+    {
+        return $this->currentLevel;
+    }
+
+    /**
+     * Set the value of currentLevel
+     *
+     * @param  int  $currentLevel
+     *
+     * @return  self
+     */ 
+    public function setCurrentLevel(int $currentLevel)
+    {
+        $this->currentLevel = $currentLevel;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of locationId
+     *
+     * @return  int
+     */ 
+    public function getLocationId()
+    {
+        return $this->locationId;
+    }
+
+    /**
+     * Set the value of locationId
+     *
+     * @param  int  $locationId
+     *
+     * @return  self
+     */ 
+    public function setLocationId(int $locationId)
+    {
+        $this->locationId = $locationId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of trainerId
+     *
+     * @return  int
+     */ 
+    public function getTrainerId()
+    {
+        return $this->trainerId;
+    }
+
+    /**
+     * Set the value of trainerId
+     *
+     * @param  int  $trainerId
+     *
+     * @return  self
+     */ 
+    public function setTrainerId(int $trainerId)
+    {
+        $this->trainerId = $trainerId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of isArchived
+     *
+     * @return  bool
+     */ 
+    public function getIsArchived()
+    {
+        return $this->isArchived;
+    }
+
+    /**
+     * Set the value of isArchived
+     *
+     * @param  bool  $isArchived
+     *
+     * @return  self
+     */ 
+    public function setIsArchived(bool $isArchived)
+    {
+        $this->isArchived = $isArchived;
+
+        return $this;
+    }
 }
